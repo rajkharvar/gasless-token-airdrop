53ec10f690568c3b00f6be21e5918cdefb54effe
rajkharvar <rajkharvar1808@gmail.com>
2022-06-19 02:14:41 +0530

    Setup for etherscan verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 const MUMBAI_PROVIDER_URL = process.env.MUMBAI_PROVIDER_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -16,5 +18,8 @@ module.exports = {
       url: MUMBAI_PROVIDER_URL,
       accounts: [PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
